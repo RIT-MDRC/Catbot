82f16b950fe8d25789b18669a700f878fd5e7836
thatnoobles <danildonchuk@gmail.com>
2023-09-24 19:09:36 -0400

    updated rpi pot class to reflect new architecture

--- src/raspi/io/potentiometer/pot.py
+++ src/raspi/io/potentiometer/pot.py
@@ -1,21 +1,10 @@
-from enum import Enum
-
-class LimitStatus(Enum):
-    """
-    Represents whether a leg has reached its limit of range of motion on either axis.
-    """
-    NEGATIVE_LIMIT = -1
-    NOT_AT_LIMIT = 0
-    POSITIVE_LIMIT = 1
-
 class Potentiometer():
     """
     Represents a potentiometer that tracks the current abduction/adduction rotation
     of each leg.
     """
     pot_id : int
-    limit_status : LimitStatus = LimitStatus.NOT_AT_LIMIT
-    last_read_value : int = 0
+    reading : int = 0
 
     def __init__(self, pot_id : int) -> None:
         """
@@ -26,5 +15,10 @@ class Potentiometer():
         """
         self.pot_id = pot_id
 
-    # UML specifies get_id(), get_limit_status(), etc as getters for this class, but
-    # in python all fields are public anyway so do we really need that?
+    def get_position(self) -> float:
+        """
+        Gets the position (in degrees) for the potentiometer with this object's `pot_id`.
+        Returns a non-zero value under normal operation; 0 if the potentiometer has hit a hardware
+        limit.
+        """
+        return 0
