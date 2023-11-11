b0f78b82db82bdedb4801b3ddeb05564d9b971f7
thatnoobles <danildonchuk@gmail.com>
2023-11-10 19:45:51 -0500

    renamed dir to "component_io" bc python imports :(

--- src/raspi/main.py
+++ src/raspi/main.py
@@ -1,1 +1,9 @@
+from component_io.motor.motor import Motor
+import time
+
 print("Meow! This is catbot cli v2")
+
+test_motor = Motor(26, 21)
+
+test_motor.run(clockwise=False, speed_percent=15)
+time.sleep(10)
