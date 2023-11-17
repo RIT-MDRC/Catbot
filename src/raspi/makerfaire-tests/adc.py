20afeaa36d307ecc2f33891b755f99695cef7aa8
thatnoobles <danildonchuk@gmail.com>
2023-11-16 22:57:51 -0500

    attempting to fix endianness

--- src/raspi/makerfaire-tests/adc.py
+++ src/raspi/makerfaire-tests/adc.py
@@ -1,7 +1,4 @@
 from smbus2 import SMBus
-# import pigpio
-
-# pot_channel = int(input("input pot channel [0-7]: "))
 
 # The I2C address of the ADC
 ADC_I2C_ADDR = 0x49
@@ -11,25 +8,6 @@ I2C_BUS = 1
 # bits needed to retrieve data at that channel
 CHANNEL_SELECTIONS = [0b000, 0b100, 0b001, 0b101, 0b010, 0b110, 0b011, 0b111]
 
-# pi = pigpio.pi()
-# handle = pi.i2c_open(I2C_BUS, ADC_I2C_ADDR)
-
-# if handle < 0:
-#     print("i2c could not connect, exiting")
-#     exit()
-
-# command_byte = int(0b10011100)  # read CH1
-# print(f"writing command byte {command_byte}")
-
-# pi.i2c_write_byte(handle, command_byte)
-
-# while True:
-#     try:
-#         (count, data) = pi.i2c_read_device(handle, 2)
-#         print(f"received {count} bytes : {data}")
-#     except KeyboardInterrupt:
-#         print("\nended")
-
 # Open I2C connection on bus 1
 with SMBus(1) as bus:
     # selection_bits = CHANNEL_SELECTIONS[pot_channel] << 4
@@ -38,7 +16,13 @@ with SMBus(1) as bus:
 
     while True:
         try:
-            reading = bus.read_word_data(ADC_I2C_ADDR, command_byte)
+            bytes_array = bus.read_i2c_block_data(ADC_I2C_ADDR, command_byte, 2)
+
+            most_significant = bytes_array[1]
+            least_significant = bytes_array[0]
+
+            reading = (most_significant << 8) + least_significant
+
             print(f"received : {reading} / {bin(reading)}")
         except Exception:
             print("failed to read, trying again...")
