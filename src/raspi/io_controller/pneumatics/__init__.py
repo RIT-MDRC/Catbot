e41a61db79faa77286e4d97065665377a4324ca6
hiromon0125 <hiroto125takeuchi@gmail.com>
2024-01-27 17:14:42 -0500

    fix: import for pneumatics

--- src/raspi/io_controller/pneumatics/__init__.py
+++ src/raspi/io_controller/pneumatics/__init__.py
@@ -1,3 +1,3 @@
-import compressor as compressor_actions
-import pressure as pressure_actions
-import valve as valve_actions
+from compressor import compressor_actions
+from pressure import pressure_actions
+from valve import valve_actions
