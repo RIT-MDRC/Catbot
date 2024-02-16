e42fb280b731ea11b31b4a6a56703a7ffd4df25f
age7359 <age7359@g.rit.edu>
2024-02-15 19:42:34 -0500

    Revert "Update potentiometer.cpp"
    This reverts commit d680ff709402cd050aa2633f0f47e10c2687cbaa.

--- src/raspi/cpp/io_controller/potentiometer.cpp
+++ src/raspi/cpp/io_controller/potentiometer.cpp
@@ -3,13 +3,10 @@
 #define ADC_I2C_ADDR 0x43   // TODO: change this to the real I2C address of the ADC
 #define MAX_ROTATION 285    // Range of rotation of the potentiometer.
 
-Potentiometer::Potentiometer(int index) {
-    this->index = index;
-    
-    gpioSetMode(index, PI_OUTPUT);
+Potentiometer::Potentiometer(int address) {
+    // TODO: not implemented
 }
 
-float Potentiometer::getDegrees() {
-    
-    gpioGetMode()
+int Potentiometer::getDegrees() {
+    // TODO: not implemented
 }
