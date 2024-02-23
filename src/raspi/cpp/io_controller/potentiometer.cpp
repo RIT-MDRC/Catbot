607d309e43f1a23b46f5788445d0fac402d461ad
thatnoobles <danildonchuk@gmail.com>
2024-02-22 20:09:10 -0500

    migrating pot code to this branch

---
+++ src/raspi/cpp/io_controller/potentiometer.cpp
@@ -0,0 +1,24 @@
+#include "potentiometer.h"
+
+Potentiometer::Potentiometer(int adcHandle, int index) {
+    this->adcHandle = adcHandle;
+    this->index = index;
+    
+    gpioSetMode(index, PI_OUTPUT);
+}
+
+unsigned int Potentiometer::getDegrees() {
+    char readBuffer[2];
+
+    // 0b1100 represents the least significant bits of the command byte, where 11 is our
+    // power-down selection and 00 are placeholders for unused bits.
+    uint8_t commandByte = (CHANNEL_SELECTION_MSB[index] << 4) + 0b1100;
+    i2cWriteByte(adcHandle, commandByte);
+
+    i2cReadDevice(adcHandle, readBuffer, 2);
+
+    uint16_t reading = (readBuffer[0] << 8) + readBuffer[1];
+    unsigned int degrees = (reading / pow(2, ADC_RESOLUTION_BITS)) * MAX_ROTATION;
+
+    return degrees;
+}
---
+++ src/raspi/cpp/io_controller/potentiometer.h
@@ -0,0 +1,39 @@
+#include <pigpio.h>
+#include <cmath>
+
+#define I2C_BUS 1
+#define ADC_I2C_ADDR 0x48   // TODO: change this to the real I2C address of the ADC
+#define ADC_RESOLUTION_BITS 12
+#define MAX_ROTATION 285.0    // Range of rotation of the potentiometer, in degrees.
+
+class Potentiometer {
+
+    public:
+        /**
+         * Creates an instance of a potentiometer. There should only be one instance of this per real-world potentiometer.
+         * index: The index of this potentiometer [0-7]. This is labeled on the circuit board as POT0, POT1, ...
+        */
+        Potentiometer(int adcHandle, int index);
+        /**
+         * Gets the current rotation of the potentiometer.
+         * returns: The current rotation of the potentiometer, in degrees [0-285].
+        */
+        unsigned int getDegrees();
+
+    private:
+        int adcHandle;
+        int index;
+
+        // 4 most significant bits for ADC command byte, used to select channel.
+        // (see page 11 of https://www.ti.com/lit/ds/symlink/ads7828.pdf)
+        const uint8_t CHANNEL_SELECTION_MSB[8] = {
+            0b1000,  // CH0
+            0b1100,  // CH1
+            0b1001,  // ...
+            0b1101,
+            0b1010,
+            0b1110,
+            0b1011,
+            0b1111   // CH7
+        };
+};
