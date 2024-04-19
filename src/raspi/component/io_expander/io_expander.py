a458b549fdf6b51e2c0dea7c0da7e28cbafa9234
Cam on Catbot RPI <cvr8924@rit.edu>
2024-04-18 20:34:00 -0400

    io expander basically done, small bug though

--- src/raspi/component/io_expander/io_expander.py
+++ src/raspi/component/io_expander/io_expander.py
@@ -1,5 +1,7 @@
-import logging
 from dataclasses import dataclass
+from adafruit_mcp230xx.mcp23017 import MCP23017
+import board
+import busio
 from gpiozero import DigitalInputDevice
 
 from component.smbus import smbus_actions
@@ -9,7 +11,7 @@ from state_management import (
     device,
     device_action, 
     device_parser,
-    identifier, 
+    identifier,
     input_device_ctx
 )
 
@@ -25,12 +27,8 @@ def is_limit_switch_active(device: DigitalInputDevice) -> bool:
     return device.is_active
 
 @device_action(limit_ctx)
-def on_limit_switch_activate(device: DigitalInputDevice, action: callable) -> bool:
+def on_limit_switch_state_change(device: DigitalInputDevice, action: callable) -> bool:
     device.when_activated = action
-
-@device_action(limit_ctx)
-def on_limit_switch_deactivate(device: DigitalInputDevice, action: callable) -> bool:
-    device.when_deactivated = action
 
 @device
 @dataclass
@@ -39,21 +37,25 @@ class IOExpander:
     16-bit I/O expander
     """
     address: int
+    mcp: MCP23017
     _identifier: str
-    i2c: smbus_actions.SMBus = identifier(smbus_actions.ctx)
 
-    def read_data(self, register: list, start_register=0x00):
-        """
-        Get which limit switches are currently active, if any.
-        """
-        ...
+    def get_interrupt_switches(self):
+        return self.mcp.int_flaga
 
 ctx = create_context("io_expander", IOExpander)
 
 @device_parser(ctx)
-def parse_io_expander(config: dict):
-    """
-    TODO: write these docs
-    """
+def parse_io_expander(config: dict) -> IOExpander:
+    mcp = MCP23017(busio.I2C(board.SCL, board.SDA), address=0x20)
+    config["mcp"] = mcp
+
+    mcp.io_control = 0b0110010
+    mcp.gppua = 0x00
+    mcp.ipola = 0x00
+    mcp.interrupt_enable = 0xff
+    mcp.clear_ints()
+    print(config)
+
     io_expander = IOExpander(**config)
     return io_expander
--- src/raspi/samples/io_expander.py
+++ src/raspi/samples/io_expander.py
@@ -1,29 +1,16 @@
-# from time import sleep
-
-# from component.io_expander import io_expander_actions
-# from state_management import configure_device
-
-# def limit_hit():
-#     print("limit switch hit")
-
-# configure_device("src/raspi/pinconfig.json")
-
-# io_expander_actions.on_limit_switch_activate("interrupt_a1", limit_hit)
-
-# while True:
-#     sleep(0.01)
-
 from time import sleep
 
-from gpiozero import DigitalInputDevice
+from component.io_expander import io_expander_actions
+from state_management import configure_device
 
-def limit_hit():
-    print("limit switch hit")
+def trigger():
+    print("a limit switch state change has occurred")
+    # ioe = io_expander_actions.parse_io_expander("io_expander_1")
+    # ioe.mcp.clear_ints()
 
-interrupt_pin = DigitalInputDevice(8)
-interrupt_pin.when_activated = limit_hit
+configure_device("src/raspi/pinconfig.json")
 
-print("configured interrupt pin")
+io_expander_actions.on_limit_switch_state_change("interrupt_a1", trigger)
 
 while True:
-    sleep(0.01)
+    ...
